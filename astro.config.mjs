3fbd423c11e588f8d69c8db6a2f8925b26ba021f
Angelito91 <portuondoangel@gmail.com>
2025-07-02 20:35:05 -0400

    remove .github and  biome then add .env and astro-min

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,23 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+
+import min from "astro-min";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://xitry-game.com/",
+
 	vite: {
 		plugins: [tailwindcss()],
 		build: {
 			minify: true,
 			cssMinify: true,
-			
+
 		},
 	},
+
 	i18n: {
 		locales: ["en", "es", "fr"],
 		defaultLocale: "es",
@@ -21,4 +25,12 @@ export default defineConfig({
 			prefixDefaultLocale: false,
 		},
 	},
+
+	env: {
+		schema: {
+			WEB3FORM_KEY: envField.string({ context: "client", access: "public" })
+		}
+	},
+
+	integrations: [min()]
 });
