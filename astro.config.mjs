d8019b4cb8b7d086c298e20c0c86ee9c7d0cd31f
Angelito <111069220+Angelito91@users.noreply.github.com>
2025-07-05 22:09:03 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,11 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://xitry-game.com/",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
   integrations: [sitemap()],
 
   vite: {
