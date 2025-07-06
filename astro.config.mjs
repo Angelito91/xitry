ceb67dc5304a1cc04336b6f79bda22ff6b82dcb2
Angelito91 <portuondoangel@gmail.com>
2025-07-05 21:49:08 -0400

    change to server , add cloudflare's adapter and @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,34 +3,35 @@ import { defineConfig, envField } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import min from "astro-min";
+import cloudflare from "@astrojs/cloudflare";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://xitry-game.com/",
+  site: "https://xitry-game.com/",
+  adapter: cloudflare(),
+  integrations: [sitemap()],
 
-	vite: {
-		plugins: [tailwindcss()],
-		build: {
-			minify: true,
-			cssMinify: true,
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      minify: true,
+      cssMinify: true
+    },
+  },
 
-		},
-	},
+  i18n: {
+    locales: ["en", "es", "fr"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: false
+    },
+  },
 
-	i18n: {
-		locales: ["en", "es", "fr"],
-		defaultLocale: "es",
-		routing: {
-			prefixDefaultLocale: false,
-		},
-	},
-
-	env: {
-		schema: {
-			WEB3FORM_KEY: envField.string({ context: "client", access: "public" })
-		}
-	},
-
-	integrations: [min()]
+  env: {
+    schema: {
+      WEB3FORM_KEY: envField.string({ context: "client", access: "public" })
+    }
+  }
 });
